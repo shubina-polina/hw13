5d2535aeeb3bdb82af66b63830698c7a2b457b2d
Fane Keat <02032002t@gamil.com>
2024-11-20 21:21:24 +0300

    new homework

--- src/Author.java
+++ src/Author.java
@@ -1,6 +1,8 @@
+import java.util.Objects;
+
 public class Author {
-    private  String firstName;
-    private  String lastName;
+    private String firstName;
+    private String lastName;
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
@@ -15,4 +17,27 @@ public class Author {
         return lastName;
     }
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
 }
+
+
+
+
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private int year;
@@ -10,7 +12,6 @@ public class Book {
 
 
     }
-
 
 
     public String getName() {
@@ -35,5 +36,27 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, year, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                ", author=" + author +
+                '}';
     }
 }
